2b80c7fb141b6b0ef1aca5f6733ec21e6795e47a
Tom Atwell <tatwell@gmail.com>
2018-03-04 08:43:09 -0800

    Centers GUI on screen.

--- window-sill/config/app.py
+++ window-sill/config/app.py
@@ -1,3 +1,5 @@
 
 TITLE = 'Window Sill'
 DESCRIPTION = 'Tempting and forbidden like py on a window sill.'
+HEIGHT = 300
+WIDTH = 500
--- window-sill/gui/tk_gui.py
+++ window-sill/gui/tk_gui.py
@@ -6,15 +6,31 @@ import config
 class TinkerGUI(object):
     def __init__(self, **options):
         title = options.get('title', config.app.TITLE)
+        width = options.get('width', config.app.WIDTH)
+        height = options.get('height', config.app.HEIGHT)
 
         self.window = self._init_window()
+        self.screen_width = self.window.winfo_screenwidth()
+        self.screen_height = self.window.winfo_screenheight()
+
         self.set_title(title)
+        self.set_window_size(width, height)
 
     def loop(self):
         self.window.mainloop()
 
     def set_title(self, title):
         self.window.title(title)
+        return self
+
+    def set_window_size(self, width, height):
+        geometry_f = "{w}x{h}+{x}+{y}"
+        x = int((self.screen_width/2) - (width/2))
+        y = int((self.screen_height/2) - (height/2))
+
+        geometry = geometry_f.format(w=width, h=height, x=x, y=y)
+        self.window.geometry(geometry)
+        return self
 
     def _init_window(self):
         return Tk()
